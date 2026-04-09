5c37b97bd3be5b9cb99698d843d0ab07af0894f1
marcussides <mrsides25@gmail.com>
2026-04-09 11:40:16 -0400

    Tests were implemented for all methods in UniqueCollection.kt

--- app/src/test/java/com/example/testingtesting123/UniqueCollectionTest.kt
+++ app/src/test/java/com/example/testingtesting123/UniqueCollectionTest.kt
@@ -16,13 +16,26 @@ class UniqueCollectionTest {
     // TODO 1: Write a test to ensure items can be added to the collection
     @Test
     fun addAnItem() {
-
+        val original = collection.size()
+        collection.addItem(Item("Granny Smith Apple"))
+        val newSize = collection.size()
+        assert(collection.get(0) == Item("Granny Smith Apple"))
+        assert(original != newSize)
+        assert(newSize == 1)
+        collection.addItem(Item("2nd Item"))
+        assert(collection.size() == 2)
     }
 
     // TODO 2: Write a test to ensure that only unique items can be added to the collection
     // Uniqueness is determined by the Item.name property, which is set via the constructor
     @Test
     fun addUniqueItem() {
+        collection.addItem(Item("Banana"))
+        collection.addItem(Item("Banana"))
+        collection.addItem(Item("BANANA"))
+        collection.addItem(Item("banana"))
+        assert(collection.size() == 1)
+
 
     }
 
@@ -37,5 +50,14 @@ class UniqueCollectionTest {
         val newSize = collection.size();
 
         assert(originalSize == 2 && newSize == 0) {"Items not cleared"}
+    }
+
+    @Test
+    fun removeAnItem(){
+        collection.addItem(Item("item1"))
+        collection.remove(Item("item1"))
+        assert(collection.size() == 0)
+        collection.remove(Item("item1"))
+        assert(collection.size() == 0)
     }
 }
